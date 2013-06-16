fdb8bf42f7b34226f7ac04008f769808c5d7b598
Alexey <mrfaithya@gmail.com>
2013-06-16 18:25:22 +0400

    new debug format.

--- src/utils/debug.hpp
+++ src/utils/debug.hpp
@@ -7,7 +7,7 @@ namespace nls{
                 #else
                         #define ___FUNC___ __FUNCTION__
                 #endif
-                #define DBG_TRACE(format,...) fprintf(stdout,"file:%s line:%d func:%s msg:" format "\n", __FILE__,__LINE__,___FUNC___, ##__VA_ARGS__)
+                #define DBG_TRACE(format,...) fprintf(stdout,"[\"%s\":%d][%s][" format "]\n", __FILE__,__LINE__,___FUNC___, ##__VA_ARGS__)
         #else
                 #define DBG_TRACE(a,...) ((void)0)
         #endif
